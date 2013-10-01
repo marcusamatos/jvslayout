130021b4a780e52a2700ee797cbfe6dee972a7fa
Marcus Matos <marcusamatos@iMacDev.local>
2013-10-01 15:57:40 -0300

    fist commit

---
+++ Module.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace JvsLayout;
+
+use Zend\ModuleManager\Feature;
+
+class Module implements Feature\AutoloaderProviderInterface
+{
+
+    /**
+     * Return an array for passing to Zend\Loader\AutoloaderFactory.
+     *
+     * @return array
+     */
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\ClassMapAutoloader' => array(
+                __DIR__ . '/autoload_classmap.php',
+            ),
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+}
---
+++ src/JvsLayout/EventListener/LayoutRouteListener.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace JvsLayout\EventListener;
+
+use Zend\EventManager\EventManagerInterface;
+use Zend\EventManager\ListenerAggregateInterface;
+use Zend\Mvc\MvcEvent;
+
+class LayoutRouteListener implements ListenerAggregateInterface {
+
+    protected $listeners = array();
+
+    /**
+     * Attach one or more listeners
+     *
+     * Implementors may add an optional $priority argument; the EventManager
+     * implementation will pass this to the aggregate.
+     *
+     * @param EventManagerInterface $events
+     *
+     * @return void
+     */
+    public function attach(EventManagerInterface $events)
+    {
+        $sharedManager = $events->getSharedManager();
+        $sharedManager->attach('Zend\Mvc\Controller\AbstractActionController', 'dispatch', array($this, 'layoutChange'), 100);
+    }
+
+    /**
+     * Detach all previously attached listeners
+     *
+     * @param EventManagerInterface $events
+     *
+     * @return void
+     */
+    public function detach(EventManagerInterface $events)
+    {
+        foreach ($this->listeners as $index => $listener) {
+            if ($events->detach($listener)) {
+                unset($this->listeners[$index]);
+            }
+        }
+    }
+
+    public function layoutChange(MvcEvent $event)
+    {
+
+        $routeMatch = $event->getRouteMatch();
+
+        //get routename position one exploded /
+        $layoutNameOne = explode('/',$routeMatch->getMatchedRouteName());
+        $layoutNameOne = $layoutNameOne[0];
+
+        //get routename position one exploded -
+        $layoutNameTwo = explode('-', $layoutNameOne);
+        $layoutNameTwo = $layoutNameTwo[0];
+
+        $config = $event->getParam('application')->getConfig();
+
+        $templateMap = $config['view_manager']['template_map'];
+
+        if(isset($templateMap['layout/' . $layoutNameOne ]))
+        {
+            $event->getViewModel()->setTemplate('layout/' . $layoutNameOne);
+        }else if(isset($templateMap['layout/' . $layoutNameTwo ]))
+        {
+            $event->getViewModel()->setTemplate('layout/' . $layoutNameTwo);
+        }
+
+    }
+}
